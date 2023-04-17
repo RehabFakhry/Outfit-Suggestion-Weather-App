02d5d354b451f7835201c3ee76f40c90d00d868b
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-17 06:04:18 +0200

    create class sharedPreferences

---
+++ app/src/main/java/com/example/outfitsuggestionweatherapp/utils/ImagePreferenceManager.kt
@@ -0,0 +1,80 @@
+package com.example.outfitsuggestionweatherapp.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.outfitsuggestionweatherapp.R
+
+class ImagePreferenceManager(context: Context) {
+    companion object {
+        private const val PREF_NAME = "image_pref"
+        private const val KEY_IMAGES = "images"
+//        private const val KEY_DATE = "date"
+    }
+
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+        PREF_NAME, Context.MODE_PRIVATE
+    )
+
+    fun saveImages(images: List<Int>) {
+        val imageIds = images.joinToString(",") { it.toString() }
+        sharedPreferences.edit().putString(KEY_IMAGES, imageIds).apply()
+//        sharedPreferences.edit().putLong(KEY_DATE , System.currentTimeMillis()).apply()
+    }
+
+    fun getRandomImage(): Int? {
+        val images = getSavedImages()
+        return if (images.isNotEmpty()) {
+            images.random()
+        } else null
+    }
+
+    private fun getSavedImages(): List<Int> {
+        val drawableIds = listOf(
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_4,
+            R.drawable.image_8
+        )
+
+        val savedImagesString = sharedPreferences.getString(KEY_IMAGES, "")
+        val savedImageIds =
+            savedImagesString?.split(",")?.mapNotNull { it.toIntOrNull() } ?: emptyList()
+        val unsavedImageIds = drawableIds.filterNot { savedImageIds.contains(it) }
+        val allImageIds = if (unsavedImageIds.isEmpty()) savedImageIds
+        else {
+            savedImageIds + unsavedImageIds.take(unsavedImageIds.size - 1)
+        }
+        saveImages(allImageIds)
+        return allImageIds
+    }
+}
+
+//    fun getRandomImage(): Int? {
+//        val images = getSavedImages()
+//        val usedImagesIds = sharedPreferences.getString(KEY_IMAGES, "")?.
+//        split(",")?.mapNotNull { it.toIntOrNull() } ?: emptyList()
+//        val currentDate = System.currentTimeMillis()
+//
+//        // If today's date is different from the saved date, reset used images
+//        if (!isSameDay(currentDate, sharedPreferences.getLong(KEY_DATE, 0))) {
+//            sharedPreferences.edit().putString(KEY_IMAGES, "").apply()
+//        }
+//
+//        // Filter out used images
+//        val availableImages = images.filterNot { usedImagesIds.contains(it)}
+//
+//        return if (availableImages.isNotEmpty()) {
+//            availableImages.random()
+//        }
+//        else null
+//    }
+//
+//    // Helper method to check if two timestamps represent the same day
+//    private fun isSameDay(timestamp1: Long, timestamp2: Long): Boolean {
+//        val calender1 = Calendar.getInstance()
+//        val calender2 = Calendar.getInstance()
+//        calender1.timeInMillis = timestamp1
+//        calender2.timeInMillis = timestamp2
+//        return calender1.get(Calendar.YEAR) == calender2.get(Calendar.YEAR) &&
+//                calender1.get(Calendar.DAY_OF_YEAR) == calender2.get(Calendar.DAY_OF_YEAR)
+//    }
