ec483798b0af0bf59276c8a2655acac31d39132b
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-18 13:43:15 +0200

    add outfit images and lotties animations

---
+++ app/src/main/java/com/example/outfitsuggestionweatherapp/data/source/ImageOutfits.kt
@@ -0,0 +1,43 @@
+package com.example.outfitsuggestionweatherapp.data.source
+
+import com.example.outfitsuggestionweatherapp.R
+
+object ImageOutfits {
+    val winterOutfits = listOf(
+        R.drawable.image_winter_1,
+        R.drawable.image_winter_2,
+        R.drawable.image_winter_3,
+        R.drawable.image_winter_4,
+        R.drawable.image_winter_5
+    )
+
+    val springOutfits = listOf(
+        R.drawable.image_spring_1,
+        R.drawable.image_spring_2,
+        R.drawable.image_summer_3,
+        R.drawable.image_summer_5,
+        R.drawable.image_spring_5,
+    )
+
+    val summerOutfits = listOf(
+        R.drawable.image_summer_1,
+        R.drawable.image_summer_2,
+        R.drawable.image_summer_3,
+        R.drawable.image_summer_4,
+        R.drawable.image_summer_5
+    )
+
+    val autumnOutfits = listOf(
+        R.drawable.image_autumn_1,
+        R.drawable.image_autumn_2,
+        R.drawable.image_autumn_3,
+        R.drawable.image_autumn_4
+    )
+}
+
+object LottieAnimations {
+    const val winterAnimation = R.raw.winter
+    const val springAnimation = R.raw.spring
+    const val summerAnimation = R.raw.sunny
+    const val autumnAnimation = R.raw.autumn
+}
