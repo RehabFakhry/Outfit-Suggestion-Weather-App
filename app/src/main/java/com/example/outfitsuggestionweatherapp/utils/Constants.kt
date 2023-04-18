0865614793088df712a3c2dcca178bb2a5d2b22d
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-18 13:49:47 +0200

    add constants

---
+++ app/src/main/java/com/example/outfitsuggestionweatherapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.outfitsuggestionweatherapp.utils
+
+object Constants {
+    const val LOCATION_PERMISSION_REQUEST_CODE: Int = 1234
+    const val API_KEY = "545c025aec18c326d80f41ac17e459b0"
+    const val BASE_URL =
+        "https://api.openweathermap.org/data/2.5/weather?lat="
+}
